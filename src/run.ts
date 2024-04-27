a6810a48453ef69eae68a368115cd397e0b36f78
csbalazs-well <122282103+csbalazs-well@users.noreply.github.com>
2024-04-27 09:10:24 +0200

    add exporter functions to run.ts file

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,41 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import * as glob from "@actions/glob";
-import { readFileSync } from 'fs';
+// import * as glob from "@actions/glob";
+import { glob } from 'glob';
+
+import { parse, matchFile } from 'codeowners-utils';
+import * as fs from 'fs';
+import { DiagConsoleLogger, DiagLogLevel, diag, metrics } from '@opentelemetry/api';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
+import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const OTEL_COLLECTOR_URL = 'http://otel-gateway-collector-opentelemetry-collector.observability.svc:4318/v1/metrics';
+// TODO make it work for Gluteus
+
+// const RUNNER_ROOT_PATH = '/home/runner/work/Maximus/Maximus/';
+const RUNNER_ROOT = '/home/runner/work'
+var SERVICE = '';
+
+// TODO is this needed?
+// originally we created the histogram object
+type Histrogram = {
+  pct: any;
+  total: any;
+  covered: any;
+  // skipped: any;
+};
+
+// const histograms = {
+//   pct: null,
+//   total: null,
+//   covered: null,
+//   // skipped: null,
+// };
+
+var histogram: Histrogram;
+
 
 interface Input {
   token: string;
@@ -9,23 +43,142 @@ interface Input {
   coverageFolder: string;
 }
 
-// this is probably not needed
-type TestType = 'unit' | 'integration' | 'e2e';
-
 type Summary = {
   path: string;
   summary: any;
-  testType: string;
 };
 
+const CODEOWNERS_TEAM_PREFIX = '@wellhealthinc/';
+const codeOwners = parse(fs.readFileSync('CODEOWNERS', { encoding: 'utf8', flag: 'r' }));
 
-export function initExporter(serviceName: string): void {}
+export function getOwnerTeam(path) {
+  if (path !== '') {
+    const entry = matchFile(path, codeOwners);
+    if (entry) {
+      const team = entry.owners.find((e) => e.startsWith(CODEOWNERS_TEAM_PREFIX));
 
-export function getCoverageSummaries(coverageFolder: string): Summary[] {return []}
+      if (team) {
+        return team.replace(CODEOWNERS_TEAM_PREFIX, '');
+      }
+    }
+  }
 
-export function recordAllCoverages(summary: any, testType: string): void {}
+  return 'UNOWNED';
+}
 
-export function shutdownExporter(): void {} 
+const SUMMARY_FILE_NAME = 'coverage-summary.json';
+
+export function getCoverageSummaries(coverageFolder :string): Summary[] {
+  const files = glob.sync(coverageFolder + '**/' + SUMMARY_FILE_NAME, {});
+  const summaries = new Array<Summary>;
+
+  if (files.length === 0) {
+    console.error('No summary files found.');
+  } else {
+    files.forEach((path) => {
+      const summary: Summary = JSON.parse(fs.readFileSync('./' + path, 'utf-8'));
+      if (hasCoverageData(summary)) {
+        summaries.push({ summary, path });
+      } else {
+        console.log(`File ${path} has no test coverage data`);
+      }
+    });
+  }
+
+  return summaries;
+}
+
+function hasCoverageData(summary) {
+  return summary.total.lines.pct !== 'Unknown';
+}
+
+export function initExporter(serviceName: string): void {
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
+
+  const meterProvider = new MeterProvider({
+    readers: [
+      new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter({
+          url: OTEL_COLLECTOR_URL,
+        }),
+        exportIntervalMillis: 1000,
+      }),
+    ],
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+  metrics.setGlobalMeterProvider(meterProvider);
+
+  const meter = meterProvider.getMeter('test_coverage');
+
+  histogram.pct = meter.createHistogram('test_coverage_percentage', {
+    description: 'Code coverage - percentage code covered',
+  });
+  histogram.total = meter.createHistogram('test_coverage_total', {
+    description: 'Code coverage - total lines of code',
+  });
+  histogram.covered = meter.createHistogram('test_coverage_covered', {
+    description: 'Code coverage - covered lines of code',
+  });
+  // histograms.skipped = meter.createHistogram('test_coverage_skipped', {
+  //   description: 'Code coverage - skipped lines of code',
+  // });
+}
+
+export function recordAllCoverages(summary) {
+  Object.keys(summary).forEach((key) => {
+    if (key !== 'total') {
+      const path = key.replace(`${RUNNER_ROOT}/${SERVICE}/${SERVICE}/`, '');
+      recordCoveragesForPath(summary[key], {
+        coverage_path: path,
+        owner_team: getOwnerTeam(path),
+        application_name: getApplicationName(path),
+      });
+    }
+  });
+}
+
+export function shutdownExporter() {
+  const meterProvider = metrics.getMeterProvider();
+  // I had to ignore these as these functions were not found on the MeterProvider
+  // Might worth investigation but it is working sooo... ¯\_(ツ)_/¯  
+  // @ts-ignore
+  meterProvider.forceFlush().then(() => { 
+    console.log('Meter provider flushed');
+     // @ts-ignore 
+    meterProvider.shutdown().then(() => {
+      console.log('Meter provider shut down');
+    });
+  });
+}
+function recordSingle(value, attributes) {
+  Object.keys(histogram).forEach((key) => {
+    histogram[key].record(value[key], attributes);
+  });
+}
+
+function recordCoveragesForPath(coverages, attributes) {
+  recordSingle(coverages.lines, { ...attributes, coverage_type: 'lines' });
+  recordSingle(coverages.statements, { ...attributes, coverage_type: 'statements' });
+  recordSingle(coverages.functions, { ...attributes, coverage_type: 'functions' });
+  recordSingle(coverages.branches, { ...attributes, coverage_type: 'branches' });
+}
+
+// This is probably only for Maximus
+function getApplicationName(path) {
+  if (path != '') {
+    let pathWithoutApps = path.indexOf('apps/') !== -1 ? path.replace('apps/', '') : path;
+    return pathWithoutApps.split('/')[0];
+  } else {
+    return null;
+  }
+}
+
+
+// --------------------------------------- Action  ---------------------------------------
+// ---------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------
 
 export function getInputs(): Input {    
     const result = {} as Input;
@@ -36,8 +189,6 @@ export function getInputs(): Input {
   }
 
   export const runAction = async (input: Input): Promise<void> => {
-    console.log('hello from csabi action')
-
     initExporter(input.serviceName);
     console.log('Meter provider created, recording coverage');
 
@@ -45,19 +196,19 @@ export function getInputs(): Input {
 
     summaries.forEach((summary) => {
       console.log(`Processing file. Path: ${summary.path}`);
-      recordAllCoverages(summary.summary, summary.testType);
+      recordAllCoverages(summary.summary);
     });
 
     console.log('Coverage recorded');
 
     shutdownExporter();
-
 }
 
 
 const run = async (): Promise<void> => {
     try {
       const input = getInputs();
+      SERVICE = input.serviceName;
       return runAction(input);
     } catch (error) {
       core.startGroup(error instanceof Error ? error.message : JSON.stringify(error));
